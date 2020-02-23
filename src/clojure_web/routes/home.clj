e53a897a7da95498064c897a661f439ac95acd9a
ningDr <ning.dr@foxmail.com>
2020-02-23 11:55:08 +0800

    source-paths

--- src/clojure_web/routes/home.clj
+++ src/clojure_web/routes/home.clj
@@ -37,29 +37,50 @@
 ;; 并提供一个表单用来创建新的消息
 (defn home [& [name message error]]
   (println "启动repl/start后，自动打开浏览器，请求根路径...")
-  (layout/common 
-   [:div.hell
-    [:h1 "clojure-web111" " " (session/get :user)]]  ;; (session/get :user)获取会话中的用户ID；session只能在请求上下文时使用
-   [:p "Welcome to my clojure-web "]
-   [:p error]
-   ;; 调用show-guests函数，创建已有的消息列表
-   (show-guests)
-   [:hr]
-   ;; 我们创建一个具有name和message两个字段的表单
-   ;; 当表单发送到服务器时，它们将会以同样的名字发送
-   (form-to [:post "/"]
-            [:p "Name:"]
-            (text-field "name" name)
-            [:p "Message:"]
-            (text-area {:rows 10 :cols 40} "message" message)
-            [:br]
-            (submit-button "comment"))
-   (println "home结束了")
+  (layout/common
+  ;  [:div.hell.text-bule-700
+  ;   [:h1 "clojure-web111" " " (session/get :user)]]  ;; (session/get :user)获取会话中的用户ID；session只能在请求上下文时使用
+  ;  [:p "Welcome to my clojure-web "]
+  ;  [:p error]
+  ;  ;; 调用show-guests函数，创建已有的消息列表
+  ;  (show-guests)
+  ;  [:hr]
+  ;  ;; 我们创建一个具有name和message两个字段的表单
+  ;  ;; 当表单发送到服务器时，它们将会以同样的名字发送
+  ;  (form-to [:post "/"]
+  ;           [:p "Name:"]
+  ;           (text-field "name" name)
+  ;           [:p "Message:"]
+  ;           (text-area {:rows 10 :cols 40} "message" message)
+  ;           [:br]
+  ;           (submit-button "comment"))
+  ;  (println "home结束了")
    #_[:form
       [:p "Name:"]
       [:input]
       [:p "Message:"]
-      [:textarea {:rows 10 :cols 40}]]))
+      [:textarea {:rows 10 :cols 40}]]
+   
+   [:div {:class "w-full container max-w-xs mx-auto"}
+    [:form {:class "bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"}
+     [:div.mb-4
+      [:label {:for "username" :class "block text-gray-700 text-sm font-bold mb-2"} "Username"]
+      [:input {:id "username" :type "text" :placeholder "Username"
+               :class "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight
+                       fous:outline-none focus:shadow"}]
+      [:div.mb-6
+       [:label {:for "password" :class "block text-gray-700 text-sm font-bold mb-2"} "Password"]
+       [:input {:id "password" :type "password" :placeholder "************"
+                :class "shadow apperance-none border border-red-500 rounded w-full py-2 px-3
+                        text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"}]
+       [:p {:class "text-red-500 text-xs italic"} "Please choose a password."]]
+      [:div.flex.items-center.justify-between
+       [:button {:class "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded
+                         focus:outline-none focus:shadow-outline"
+                 :type "button"} "Sign In"]
+       [:a.inline-block.align-baseline.font-bold.text-sm.text-blue-500.hover:text-blue-800 {:href "#"}
+        "Forgot Password?"]]]
+    [:p.text-center.text-gray-500.text-xs "&copy;2020 ningdr Corp. All rights reserved."]]]))
 
 (defn save-message
   "保存消息"
@@ -109,19 +130,20 @@
   [request]
   (let [_ (println request)
         body (:body request)
-        body (cset/rename-keys
-              body
-              (reduce #(merge %1 {%2 (keyword %2)})
-                      {} (keys body)))
-        board (:board body)
-        char1 \*
-        next-board (game/echart-format-json board char1)]
-    (println next-board)
-    next-board))
+        ; body (cset/rename-keys
+        ;       body
+        ;       (reduce #(merge %1 {%2 (keyword %2)})
+        ;               {} (keys body)))
+        ; board (:board body)
+        ; char1 \*
+        ; next-board (game/echart-format-json board char1)
+        ]
+    (println body "==============")
+    {:code 1 :msg "成功"}))
 
 (defn vue-test [a b]
   (println  a "***vue test***" b)
-  {:a 12})
+  {:code 1 :msg "成功"})
 
 ;; 使用defroutes来定义clojure-web.routes.home命名空间中的路由
 ;; 每个路由都代表着一个应用会响应的URI地址。
